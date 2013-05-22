c81908c917f2cfbc08e232c73093dc5a3ccedc34
milaq <micha.laqua@gmail.com>
2013-05-22 18:57:19 +0200

    initial commit

---
+++ goo.mk
@@ -0,0 +1,12 @@
+#
+# GooManager specific stuff
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.goo.developerid=milaq \
+        ro.goo.rom=cm10.1nightly \
+        ro.goo.version=$(shell date +%s)
+
+# include goo manager
+PRODUCT_COPY_FILES += \
+       vendor/goomanager/GooManager.apk:system/app/GooManager.apk
